d50a34c146e4bb5ccc95eee8cfda458e783090e1
Lauren Miller <lozinbru@gmail.com>
2026-07-24 23:12:05 +0000

    hello

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl << std::endl << std::endl;
+    return 0;
+}
