26f3e2f2a504869e48ecfe3fe16ccd20c1e6b418
Lauren Miller <lozinbru@gmail.com>
2026-08-06 23:48:16 +0000

    updates

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -9,18 +9,24 @@ int main(){
     int fahrenheit = 100;
     int celsius;
 
-    celsius = fahrenheit;
+    std::cout << typeid(static_cast<double>(static_cast<double>(fahrenheit) - 32.0)).name() << std::endl;
+
+
+    celsius = static_cast<double>((static_cast<double>(fahrenheit) - 32.0) * (5.0 / 9.0));
+
+    std::cout << typeid(celsius).name() << std::endl;
+
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
     std::cout << "Celsius   : " << celsius << std::endl;
 
-    float weight = 10.99;
+    double weight = 10.99;
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    std::cout << "Integer part   : " << static_cast<int>(weight) << std::endl;
+    std::cout << "Fractional part: " << static_cast<int>((weight - static_cast<int>(weight)) * 10000) << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
